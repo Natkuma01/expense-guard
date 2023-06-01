ef06142008aab157d9e79e92fd1fba56a37fd267
Shimin <natkuma@outloook.com>
2023-06-01 18:07:39 -0400

    Complete Feature 12

--- receipts/models.py
+++ receipts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 class ExpenseCategory(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="categories",
         on_delete=models.CASCADE)
     def __str__(self):
@@ -14,7 +15,7 @@ class Account(models.Model):
     name = models.CharField(max_length=100)
     number = models.CharField(max_length=20)
     owner = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="accounts",
         on_delete=models.CASCADE
     )
@@ -27,7 +28,7 @@ class Receipt(models.Model):
     tax = models.DecimalField(max_digits=10, decimal_places=3)
     date = models.DateTimeField(null=True)
     purchaser = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name="receipts",
         on_delete=models.CASCADE
     )
--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from receipts.views import show_receipt, create_receipt
+from receipts.views import show_receipt, create_receipt, account_list, category_list
 
 urlpatterns = [
+    path("accounts/", account_list, name="account_list"),
+    path("categories/", category_list, name="category_list"),
     path("create/", create_receipt, name="create_receipt"),
     path("", show_receipt, name="home"),
 ]
