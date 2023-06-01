8e78b7360ed971348b398232f9d414ef9d8eb1c8
Shimin <natkuma@outloook.com>
2023-06-01 18:31:10 -0400

    Complete Feature 13

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from receipts.views import show_receipt, create_receipt, account_list, category_list
+from receipts.views import show_receipt, create_receipt, account_list, category_list, create_expense_category
 
 urlpatterns = [
+    path("categories/create/", create_expense_category, name="create_category"),
     path("accounts/", account_list, name="account_list"),
     path("categories/", category_list, name="category_list"),
     path("create/", create_receipt, name="create_receipt"),
--- receipts/views.py
+++ receipts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from receipts.models import Receipt, ExpenseCategory, Account
 from django.contrib.auth.decorators import login_required
-from receipts.forms import ReceiptForm
+from receipts.forms import ReceiptForm, ExpenseCategoryForm, AccountForm
 
 @login_required
 def show_receipt(request):
@@ -43,3 +43,21 @@ def account_list(request):
         "accounts": accounts
     }
     return render(request, "receipts/accounts.html", context)
+
+@login_required
+def create_expense_category(request):
+    if request.method == "POST":
+        form = ExpenseCategoryForm(request.POST)
+        if form.is_valid():
+            expense_category = form.save(False)
+            expense_category.owner = request.user
+            form.save()
+            return redirect("category_list")
+    else:
+        form = ExpenseCategoryForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "receipts/categories/create.html", context)
+
