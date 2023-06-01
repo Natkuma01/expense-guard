c70f65b78bcd5d46b2d87f899906823cd7d2d6d7
Shimin <natkuma@outloook.com>
2023-06-01 16:44:55 -0400

    Complete Feature 11

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from receipts.views import show_receipt
+from receipts.views import show_receipt, create_receipt
 
 urlpatterns = [
+    path("create/", create_receipt, name="create_receipt"),
     path("", show_receipt, name="home"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from receipts.models import Receipt
 from django.contrib.auth.decorators import login_required
+from receipts.forms import ReceiptForm
 
 @login_required
 def show_receipt(request):
@@ -9,3 +10,20 @@ def show_receipt(request):
         "receipts": receipts,
     }
     return render(request, "receipts/main.html", context)
+
+@login_required
+def create_receipt(request):
+    if request.method == "POST":
+        form = ReceiptForm(request.POST)
+        if form.is_valid():
+            receipt = form.save(False)
+            receipt.purchaser = request.user
+            form.save()
+            return redirect("home")
+    else:
+        form = ReceiptForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "receipts/create.html", context)
